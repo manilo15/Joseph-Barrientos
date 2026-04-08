c79369ffdfa2198d098bf4eaa021268f60564aa0
manilo15 <138084745+manilo15@users.noreply.github.com>
2026-04-08 00:53:42 -0400

    Add network scanner script to ping devices

---
+++ Projects/network-scanner/scanner.py
@@ -0,0 +1,18 @@
+import os
+import socket
+
+network = "192.168.1."  
+
+print("Scanning network...\n")
+
+for i in range(1, 255):
+    ip = network + str(i)
+    response = os.system(f"ping -n 1 -w 100 {ip} > nul")
+
+    if response == 0:
+        try:
+            hostname = socket.gethostbyaddr(ip)[0]
+        except:
+            hostname = "Unknown"
+
+        print(f"{ip} is up - {hostname}")
